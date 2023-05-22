3e31afe0e11cd6542661965b3664f85219403fc3
Vasiliy <pilaroiding@gmail.com>
2023-05-22 18:39:00 +0300

    Initial commit

---
+++ src/main/java/made/myself/newarraylist/NewArrayListApplication.java
@@ -0,0 +1,13 @@
+package made.myself.newarraylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewArrayListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewArrayListApplication.class, args);
+    }
+
+}
